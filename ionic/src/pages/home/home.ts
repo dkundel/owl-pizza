a00ba47cdfd9cb5e2eeeb490460b4ea17f9857a5
Dominik Kundel <dominik.kundel@gmail.com>
2016-10-05 16:19:29 +0200

    Hide push button on some platforms and update logo

--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { OnInit, OnDestroy } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, Platform, ToastController } from 'ionic-angular';
 import { Push, PushNotification } from 'ionic-native';
 
 @Component({
@@ -11,15 +11,26 @@ import { Push, PushNotification } from 'ionic-native';
 export class HomePage implements OnInit, OnDestroy {
 
   public hasPushPermission = false;
+  public pushAvailable = false;
+
   private push: PushNotification;
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+  constructor(
+    public navCtrl: NavController, 
+    public platform: Platform,
+    private alertCtrl: AlertController, 
+    private toastCtrl: ToastController
+  ) {
     
   }
 
   ngOnInit() {
+    this.pushAvailable = this.platform.is('ios') || this.platform.is('android');
     Push.hasPermission().then(({isEnabled}) => {
       this.hasPushPermission = isEnabled;
+      if (this.hasPushPermission) {
+        this.initializePush();
+      }
     });
   }
 
@@ -95,7 +106,7 @@ export class HomePage implements OnInit, OnDestroy {
   }
 
   initializePush() {
-    if (!this.hasPushPermission) {
+    if (this.pushAvailable) {
       this.push = Push.init({
 
       });
@@ -104,16 +115,30 @@ export class HomePage implements OnInit, OnDestroy {
     }
   }
 
-  private handleFacebook(data) {
-    if (data.fbName) {
-      console.log(data.fbName);
+  private handleFacebook({ fbName }) {
+    if (fbName) {
+      console.log(fbName);
+      let msg = `We will send you a message to "${fbName}" as soon as a pizza is available.`
+      this.showMessage(msg);
     }
   }
 
-  private handlePhoneNumber(data) {
-    if (data.phoneNumber) {
-      console.log(data.phoneNumber);
+  private handlePhoneNumber({ phoneNumber }) {
+    if (phoneNumber) {
+      console.log(phoneNumber);
+      let msg = `We will send you an SMS to "${phoneNumber}" as soon as a pizza is ready.`
+      this.showMessage(msg);
     }
+  }
+
+  private showMessage(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    toast.present();
   }
 
   private registerHandlers() {
